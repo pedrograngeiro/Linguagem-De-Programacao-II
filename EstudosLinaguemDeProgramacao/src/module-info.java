9accde966d7c54f1a460454f0334f8865dcf17fe
pedrograngeiro <pedrop.grangeiro@gmail.com>
2023-10-03 09:58:51 -0300

    Passando exemplo aula 10

--- EstudosLinaguemDeProgramacao/src/module-info.java
+++ EstudosLinaguemDeProgramacao/src/module-info.java
@@ -4,5 +4,5 @@
 /**
  * 
  */
-module principal {
+module EstudosLinaguemDeProgramacao {
 }
--- EstudosLinaguemDeProgramacao/src/principal/main.java
+++ EstudosLinaguemDeProgramacao/src/principal/main.java
@@ -1,10 +1,20 @@
 package principal;
-
+import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Ola mundo!");
+		int[] numeros = new int[10];
+		int soma=0;
+		Scanner sc = new Scanner(System.in);
+		for(int i = 0; i < numeros.length; i++) {
+			System.out.println("Número: ");
+			numeros[i] = sc.nextInt();
+			soma = soma+ numeros[i];
+		}
+		
+		System.out.println("Soma: " + soma);
+		sc.close();
 	}
 
 }
