948a030b8a831b1d0a0cdb670519b39492045a78
pedrograngeiro <pedrop.grangeiro@gmail.com>
2023-10-06 09:20:34 -0300

    Corrigindo erro de mostrar print

--- EstudosLinaguemDeProgramacao/src/principal/Supermercado.java
+++ EstudosLinaguemDeProgramacao/src/principal/Supermercado.java
@@ -10,14 +10,14 @@ public class Supermercado {
 		Produto p1 = new Produto(1,"Arroz tio joão", "kg", 3.45, 100);
 		Produto p2 = new Produto(2, "Suco uva DelValle", "Garrafa1L", 4.99,200);
 		
-		System.out.println("Código do produto: " + p1.codigo);
-		System.out.println("Nome do produto:" + p1.nome);
-		System.out.println("Unidade de médida: " + p1.unidMedida);
-		System.out.println("Preco do produto: " + p1.preco);
-		System.out.println("Estoque do produto: " + p1.estoque);
+		System.out.println("Código do produto: " + p1.getCodigo());
+		System.out.println("Nome do produto:" + p1.getNome());
+		System.out.println("Unidade de médida: " + p1.getUnidMedida());
+		System.out.println("Preco do produto: " + p1.getPreco());
+		System.out.println("Estoque do produto: " + p1.getEstoque());
 		
 		System.out.println("--------//--------");
-		System.out.println(p1.toString());
+//		System.out.println(p1.toString());
 //		
 	}
 
